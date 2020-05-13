786f41b47b110d5a101d4dc2a278922de22f3a30
김형석 <wizcore@gimhyeongseog-ui-MacBookPro.local>
2020-05-13 14:04:30 +0900

    features>index.ts 주석 설명 추가

--- src/features/index.ts
+++ src/features/index.ts
@@ -5,6 +5,7 @@ import {
     createSlice,
     PayloadAction
 } from '@reduxjs/toolkit';
+//addTodos 액션함수를 만들기위해 reduxjs/tolkit이 필요
 import { generate as generateRandomStr } from 'randomstring';
 
 /* Todo와 TodoList의 인터페이스 정의 */
@@ -35,6 +36,9 @@ const actionPrefix = "TODOS";
 
 const addTodos = createAction<object>(`${actionPrefix}/add`);
 
+/*
+todoSlice.reducer를 아까 맨 처음 만들어준 rootReducers에 넣으면 이제 자동으로 reducer 함수들이 연결된다.
+*/
 const reducers = {
     add: ({ list }: TodoList, { payload: { text, isDone } }: PayloadAction<Todo>) => {
         const newTodo: Todo = {
@@ -43,10 +47,23 @@ const reducers = {
             isDone
         };
 
-        list.push(newTodo);
+        list.push(newTodo);//list에 새로운 Todo데이터 삽입 
     },
 };
 
+/*
+    createSlice Reducer 생성
+    
+-reducers는 각 action type별로 바뀔 상태값을 정의할 reducer 객체들을 말한다.
+-initialState는 말 그대로 초기 상태 값이다.
+-name은 아까 위에서 정의한 actionPrefix 값이 들어가는데, 자동으로 action Type이 생성될 때 actionPrefix를 prefix로 사용하여 만들어진다. 
+즉, reducers 객체에 add라는 속성이 존재하면, 자동으로 `${actionPrefix}/add`('TODOS/add')라는 actionType이 생성된다.
+-extraReducers는 자동으로 actionType과 actionCreator가 생성되지 않는 reducer가 생성되며, 
+함수 내부에서 자동 생성되는 actionType이 아닌 사용자가 별도의 actionType을 사용할 수 있다. 이 속성은 꼭 사용하지 않아도 된다.
+
+
+출처: https://im-developer.tistory.com/185 [Code Playground]
+*/
 const todoSlice = createSlice({
     reducers,
     initialState,
