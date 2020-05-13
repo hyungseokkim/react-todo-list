828d5aa7d4f7cfc3d4bd45b54aad706774281213
김형석 <wizcore@gimhyeongseog-ui-MacBookPro.local>
2020-05-13 11:01:42 +0900

    초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+import {rootReducer} from './features';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
